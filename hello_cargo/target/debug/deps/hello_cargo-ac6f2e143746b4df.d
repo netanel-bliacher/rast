7f5bea9839bd54c739dde7aaa0318d95906d92d4
Netanel Bliacher <nbliacher@gmail.com>
2025-03-13 12:02:08 +0200

    comm

---
+++ hello_cargo/target/debug/deps/hello_cargo-ac6f2e143746b4df.d
@@ -0,0 +1,5 @@
+/Users/netanelbliacher/programming/rast/hello_cargo/target/debug/deps/libhello_cargo-ac6f2e143746b4df.rmeta: src/main.rs
+
+/Users/netanelbliacher/programming/rast/hello_cargo/target/debug/deps/hello_cargo-ac6f2e143746b4df.d: src/main.rs
+
+src/main.rs:
